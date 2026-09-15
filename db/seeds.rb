7706885341fe0b47426cd600daa18adeae4932be
Pablo Trajano <pablotrajjano@gmail.com>
2026-09-15 00:34:25 -0300

    Etapa 6: Alteração na view e seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+movies = [
+  {
+    title: "Toy Story",
+    rating: "G",
+    release_date: Date.new(1995, 11, 22),
+    description: "Brinquedos ganham vida."
+  },
+  {
+    title: "The Matrix",
+    rating: "R",
+    release_date: Date.new(1999, 3, 31),
+    description: "Um programador descobre a realidade."
+  },
+  {
+    title: "Star Wars",
+    rating: "PG",
+    release_date: Date.new(1977, 5, 25),
+    description: "Uma aventura em uma galáxia distante."
+  }
+]
+
+movies.each do |attributes|
+  Movie.find_or_create_by!(title: attributes[:title]) do |movie|
+    movie.assign_attributes(attributes)
+  end
+end
