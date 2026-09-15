15dd7286275304d1a0317704f9df83e068da78e7
Pablo Trajano <pablotrajjano@gmail.com>
2026-09-15 00:16:38 -0300

    Etapa 5: Regra de negócio modal Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  RATINGS = %w{G PG PG-13 R NC-17}.freeze
+
+  validates :title, presence: true
+  validates :rating, inclusion: { in: RATINGS }
+  validates :release_date, presence: true
 end
